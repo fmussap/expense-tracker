020ed951dc3e8131eebc7e43d1c017b3ec68b99a
fmussap <fabiomussap@yahoo.com>
2017-12-31 17:13:07 +0000

    config

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,6 +1,7 @@
-'use strict'
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'development'
+}
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod')
 } else if (process.env.NODE_ENV === 'test') {
