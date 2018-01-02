f2e345360de0edc193b8881de173dd0e458ef763
fmussap <fabiomussap@yahoo.com>
2018-01-02 08:51:27 +0000

    comments removed

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,4 @@
-// import Dotenv from 'dotenv'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
-// Dotenv.config({ path: '.env.test' })
 Enzyme.configure({ adapter: new Adapter() })
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,8 @@ const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-// console.log('NODE_ENV webpack before', process.env.NODE_ENV)
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } = require('./src/config/keys')
-// console.log('api', apiKey)
-// console.log('NODE_ENV webpack', process.env.NODE_ENV)
-// if (process.env.NODE_ENV === 'test') {
-//   require('dotenv').config({ path: '.env.test' })
-// } else if (process.env.NODE_ENV === 'development') {
-//   require('dotenv').config({ path: '.env.development' })
-// }
 
 module.exports = (env) => {
   const isProduction = env === 'production'
